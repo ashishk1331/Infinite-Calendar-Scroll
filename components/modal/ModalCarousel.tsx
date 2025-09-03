c4b646835e463d794228b9553bdf514acdac5852
Ashish Khare <akstyles211@gmail.com>
2025-09-03 16:04:41 +0530

    feat: modal carousel with scaling effect

--- components/modal/ModalCarousel.tsx
+++ components/modal/ModalCarousel.tsx
@@ -1,9 +1,8 @@
 "use client";
-
 import ModalCard from "./ModalCard";
 import { useJournals } from "@/context/JournalContext";
-import { Journal } from "@/types/types";
-import { useEffect, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import ClassNames from "embla-carousel-class-names";
 
 type ModalCarouselProps = {
   journalId: number;
@@ -11,20 +10,37 @@ type ModalCarouselProps = {
 
 export default function ModalCarousel({ journalId }: ModalCarouselProps) {
   const { events } = useJournals();
-  const [journals, setJournals] = useState<Journal[]>([events[journalId]]);
 
-  useEffect(() => {
-    const start = Math.max(journalId - 1, 0);
-    const end = Math.min(events.length - 1, journalId + 1);
+  const plugins = [
+    ClassNames({
+      snapped: "is-snapped",
+    }),
+  ];
 
-    setJournals(events.slice(start, end + 1))
-  }, [journalId]);
+  const [emblaRef] = useEmblaCarousel(
+    {
+      align: "center",
+      containScroll: false,
+      dragFree: false,
+      loop: false,
+      skipSnaps: false,
+      startIndex: journalId,
+    },
+    plugins,
+  );
 
   return (
-    <div className="flex w-full gap-4 items-stretch overflow-x-auto snap-x snap-mandatory scroll-smooth scrollbar-hide px-4 md:px-8">
-      {journals.map((journal, index) => (
-        <ModalCard key={journal.description + index} journal={journal} />
-      ))}
+    <div className="w-full max-w-full mx-auto" ref={emblaRef}>
+      <div className="flex">
+        {events.map((journal, index) => (
+          <div
+            key={journal.description + index}
+            className="flex-[0_0_75%] min-w-0 [.is-snapped]:scale-110 transition-transform"
+          >
+            <ModalCard journal={journal} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
